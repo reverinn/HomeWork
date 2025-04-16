0e2f2ae9157d1d912eda06a0b1a59b4fd0f55af2
PorfirioTramontana <PorfirioTramontana@users.noreply.github.com>
2025-04-16 12:24:30 +0200

    Aggiunto codice nel main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import model.Utente;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!???!");
+        Utente u= new Utente("admin", "admin");
+        System.out.println(u.getLogin());
     }
 }
